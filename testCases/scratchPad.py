577945c8ff73500226c780fc8efd3f35e4a7a671
Manish Singh <manish.uit.bu@gmail.com>
2021-08-21 17:31:57 -0400

    added try except and finally blockes in TDD test case, generated html report from CLI and added screenshot on failure

--- testCases/scratchPad.py
+++ testCases/scratchPad.py
@@ -1,6 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 from utilities import XLUtility
 from utilities.XLUtility import getRowCount
 from utilities.logGeneration import LogGen
+
 
 #
 # class ScratchPad:
@@ -17,7 +22,22 @@ from utilities.logGeneration import LogGen
 # if __name__ == '__main__':
 #     sp = ScratchPad()
 #     sp.log_check()
-path = '/home/manish/Documents/Programming/Python/pythonProject/google_Search_Automation_Framwork_Python/Test_Data' \
-       '/auto_test_data.xlsx '
-row = XLUtility.getRowCount(path, 'Sheet1')
-print(f"# of rows is {row}")
+# path = '/home/manish/Documents/Programming/Python/pythonProject/google_Search_Automation_Framwork_Python/Test_Data' \
+#        '/auto_test_data.xlsx '
+# row = XLUtility.getRowCount(path, 'Sheet1')
+# print(f"# of rows is {row}")
+
+class A:
+    def screenshottest(self):
+        option = Options()
+        option.add_argument('--no-sandbox')
+        option.add_experimental_option('useAutomationExtension', False)
+        # option.add_argument('--headless')
+        # driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = webdriver.Chrome(executable_path="/home/manish/Documents/browsers/chromedriver", options=option)
+        driver.get("https://www.facebook.com")
+        driver.save_screenshot("/home/manish/Documents/Programming/Python/pythonProject"
+                               "/google_Search_Automation_Framwork_Python/Screenshots/" + __name__ + ".png")
+        driver.close()
+
+
--- testCases/test_google_multi_search_TDD.py
+++ testCases/test_google_multi_search_TDD.py
@@ -8,8 +8,10 @@ from utilities.readConfig import ReadConfig
 class Test_002_google_multi_search:
     logger = LogGen.log()
     url = ReadConfig.get_application_url()
-    path = '/home/manish/Documents/Programming/Python/pythonProject/google_Search_Automation_Framwork_Python/' \
-           'Test_Data/test_data.xlsx'
+    path = '/home/manish/Documents/Programming/Python/pythonProject/google_Search_Automation_Framwork_Python' \
+           '/Test_Data/auto_test_data.xlsx '
+    screenshot = '/home/manish/Documents/Programming/Python/pythonProject/google_Search_Automation_Framwork_Python' \
+                 '/Screenshots '
 
     def test_google_multiSearch_method(self, setup):
 
@@ -20,39 +22,52 @@ class Test_002_google_multi_search:
 
         self.logger.info("Initializing the search page object")
         self.sp = SearchPage(self.driver)
-        self.logger.info("calling the test data from XL file")
-        self.rows = XLUtility.getRowCount(self.path, "Sheet1")
-        print(f"Number of rows in excel is {self.rows + 1}")
-        test_status = []
-        for row in range(2, self.rows + 1):
-            searchtext = XLUtility.readData(self.path, "Sheet1", row, 1)
-            exp = XLUtility.readData(self.path, "Sheet1", row, 2)
-            self.sp.set_text_to_search(searchtext)
-            title = self.driver.title
-            if 'Search' in title:
-                if exp == 'pass':
-                    self.logger.info("Title matched")
-                    test_status.append("pass")
+        try:
+
+            self.logger.info("calling the test data from XL file")
+            self.rows = XLUtility.getRowCount(self.path, "Sheet1")
+            print(f"Number of rows in excel is {self.rows + 1}")
+            test_status = []
+            for row in range(2, self.rows + 1):
+                searchtext = XLUtility.readData(self.path, "Sheet1", row, 1)
+                exp = XLUtility.readData(self.path, "Sheet1", row, 2)
+                self.sp.set_text_to_search(searchtext)
+                title = self.driver.title
+                if 'Search' in title:
+                    if exp == 'pass':
+                        self.logger.info("Title matched")
+                        test_status.append("pass")
+                        assert True
+                    elif exp == 'fail':
+                        self.logger.info("title match but expectation did not match")
+                        test_status.append("fail")
+                        assert False
+
+                elif 'Search' not in title:
+                    if exp == 'pass':
+                        self.logger.error("title did not match but it was expectation")
+                        test_status.append("pass")
+                        assert True
+                    elif exp == 'fail':
+                        self.logger.error("title did not match and it was not the expectation")
+                        test_status.append("fail")
+                        self.driver.save_screenshot(self.screenshot + __name__ + '.png')
+                        assert False
+
+                if 'fail' not in test_status:
+                    self.logger.info("*** test_google_multiSearch_method is passed ***")
+                    # self.driver.close()
                     assert True
-                elif exp == 'fail':
-                    self.logger.info("title match but expectation did not match")
-                    test_status.append("fail")
+                if 'fail' in test_status:
+                    self.logger.info("*** test_google_multiSearch_method is failed ***")
+                    # self.driver.close()
+                    self.driver.save_screenshot(self.screenshot + __name__ + '.png')
                     assert False
 
-            elif 'Search' not in title:
-                if exp == 'pass':
-                    self.logger.error("title did not match but it was expectation")
-                    test_status.append("pass")
-                    assert True
-                elif exp == 'fail':
-                    self.logger.error("title did not match and it was not the expectation")
-                    test_status.append("fail")
-                    assert False
-        if 'fail' not in test_status:
-            self.logger.info("*** test_google_multiSearch_method is passed ***")
-            self.driver.close()
-            assert True
-        if 'fail' in test_status:
-            self.logger.info("*** test_google_multiSearch_method is failed ***")
-            self.driver.close()
+        except Exception as e:
+            print("*******" + e.args + "*******")
             assert False
+
+        finally:
+            self.driver.close()
+
